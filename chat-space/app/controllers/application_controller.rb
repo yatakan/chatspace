01a3585af04a124e52ee81ee6265d1acc56afe1e
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-06-29 16:24:29 +0900

    サインイン・サインアウト機能
    after_sign_out_path_forを設定したのみです。

--- chat-space/app/controllers/application_controller.rb
+++ chat-space/app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
   before_action :authenticate_user!
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
