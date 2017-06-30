14465034bb0af610021274785b8f1fc295f935f3
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-06-30 13:21:24 +0900

    kanma
    カンマの後ろのスペース忘れを修正

--- chat-space/app/models/user.rb
+++ chat-space/app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, uniqueness: true
   has_many :groups_users
-  has_many :groups,through: :groups_users
+  has_many :groups, through: :groups_users
 end
