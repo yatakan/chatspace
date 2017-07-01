a2ce2981f1e4032e49bbe5bfffc2af573df5b233
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-07-01 16:42:45 +0900

    Make a group
    - groupsルーティングの設定
    - ja.ymlの導入
    - groups/new.html.hamlの編集
    - groups_controllerの設定

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,8 +1,27 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
+
+  def create
+    @group = Group.new(params_permit)
+    if @group.save
+      flash[:notice] = "グループが作成されました。"
+      redirect_to messages_path
+    else
+      render 'new'
+    end
+    # reirect と renderのちがい
+  end
+
   def edit
   end
+
   def update
+  end
+
+  private
+  def params_permit
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
