cdc371846d037f40b1a7f02238ba34957c99e90a
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-07-05 21:34:57 +0900

    Example name
    - exampleの名前が違ったので訂正。コピペからのケアレスミス。

--- chat-space/spec/models/message_spec.rb
+++ chat-space/spec/models/message_spec.rb
@@ -28,7 +28,7 @@ describe User do
       expect(message.errors[:group_id]).to include('を入力してください')
     end
 
-    it "is invalid without group_id?" do
+    it "is invalid without user_id?" do
       message[:user_id] = nil
       message.valid?
       expect(message.errors[:user_id]).to include('を入力してください')
